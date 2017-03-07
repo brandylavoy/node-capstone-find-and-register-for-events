c5a574c9d79a327aa8faa61ccc897017ef275b48
Brandy La Voy <brandylavoy@gmail.com>
2017-03-07 04:32:31 +0000

    folder structure set up

---
+++ config.js
@@ -0,0 +1,7 @@
+
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       (process.env.NODE_ENV === 'production' ?
+                            'mongodb://localhost/api-capstone-bookshelf' :
+                            'mongodb://localhost/api-capstone-bookshelf');
+exports.PORT = process.env.PORT || 5000;
