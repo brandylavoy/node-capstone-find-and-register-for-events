c779bbd5795b3721f7712360dae0679cf8abdd94
Brandy La Voy <brandylavoy@gmail.com>
2017-03-08 05:18:48 +0000

    css resets added

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/api-capstone-bookshelf' :
-                            'mongodb://localhost/api-capstone-bookshelf');
+                            'mongodb://localhost/node-capstone-find-register-events' :
+                            'mongodb://localhost/node-capstone-find-register-events');
 exports.PORT = process.env.PORT || 5000;
