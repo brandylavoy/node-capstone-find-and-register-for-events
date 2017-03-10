da1a03b99ff2998aef25f2edb12605233cd5112c
Brandy La Voy <brandylavoy@gmail.com>
2017-03-10 03:28:53 +0000

    updated test-active-api

--- test/test-active-api.js
+++ test/test-active-api.js
@@ -1,18 +1,67 @@
 var chai = require("chai");
 var chaiHttp = require("chai-http");
-
-var app = require("../server");
+var server = require('../server');
+var models = require('../models');
 
 var should = chai.should();
+var app = server.app;
+// var storage = server.storage;
 
 chai.use(chaiHttp);
 
-describe("/", function () {
+describe('node-capstone-find-register-events', function() {
+    before(function(done) {
+        server.runServer(function() {
+            models.create({
+                name: 'Broad beans'
+            }, {
+                name: 'Tomatoes'
+            }, {
+                name: 'Peppers'
+            }, function() {
+                done();
+            });
+        });
+    });
 
-	it("should return a 200 status code and HTML on GET", function () {
-		return chai.request(app).get("/").then(function (res) {
-			res.should.have.status(200);
-			res.should.be.html;
-		});
-	});
+    describe('index page', function() {
+        it('exists', function(done) {
+            chai.request(app)
+                .get('/')
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.should.be.html;
+                    done();
+                });
+        });
+    });
+
+    describe('event page', function() {
+        it('exists', function(done) {
+            chai.request(app)
+                .get('/')
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.should.be.html;
+                    done();
+                });
+        });
+    });
+
+    describe('dashboard page', function() {
+        it('exists', function(done) {
+            chai.request(app)
+                .get('/')
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.should.be.html;
+                    done();
+                });
+        });
+    });
+    after(function(done) {
+        models.remove(function() {
+            done();
+        });
+    });
 });
